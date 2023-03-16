7ad12ce8a894e5193d41e747e9aba3357fe4197f
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-03-17 03:56:55 +0600

    Add files via upload

---
+++ 339A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    for (int i = 0; i < s.size(); i++)
+    {
+        for (int j = 0; j < s.size() - 1; j++)
+        {
+            if (s[j] > s[j + 2])
+            {
+                swap(s[j], s[j + 2]);
+            }
+        }
+       
+    }
+     cout << s << endl;
+}
