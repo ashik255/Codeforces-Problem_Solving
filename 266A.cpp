78d9843d6efe362ae5bb60c80b7045429242b47a
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-03-18 04:01:33 +0600

    Add files via upload

---
+++ 266A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b = 0;
+    cin >> a;
+    string s;
+    cin >> s;
+    for (int i = 0; i < a; i++)
+    {
+        if (s[i] == s[i + 1])
+        {
+            b++;
+        }
+    }
+    cout << b << endl;
+}
