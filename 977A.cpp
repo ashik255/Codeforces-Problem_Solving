0f4828fee4f2a3cd678ce3a708090747c88643ab
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-03-19 03:50:26 +0600

    Add files via upload

---
+++ 977A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, n;
+    cin >> a >> n;
+    for (int i = 0; i < n; i++)
+    {
+        if (a % 10 == 0)
+            a /= 10;
+        else
+            a--;
+    }
+    cout << a;
+}
