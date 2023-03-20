208ce083781bea3c139d710558ccc83e9bb4f5f7
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-03-20 14:51:49 +0600

    Add files via upload
    Div-4 Contest Problem

---
+++ 1807A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (c - b == a)
+            cout << "+" << endl;
+        else
+            cout << "-" << endl;
+    }
+}
