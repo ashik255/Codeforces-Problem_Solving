eaec30642efa782b4a30808a7856da774e18fb4f
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-03-22 03:48:22 +0600

    Add files via upload

---
+++ 200B.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    float sum = 0;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+    cout << sum / n << endl;
+}
