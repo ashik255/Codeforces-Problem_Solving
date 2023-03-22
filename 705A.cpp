278fd219377ab017ddc79767be0d68989b6c96df
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-03-23 01:57:52 +0600

    Add files via upload

---
+++ 705A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i < n; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << "I hate that ";
+        }
+        if (i % 2 == 0)
+        {
+            cout << "I love that ";
+        }
+    }
+    if (n % 2 != 0)
+        cout << "I hate it ";
+    if (n % 2 == 0)
+        cout << "I love it ";
+}
