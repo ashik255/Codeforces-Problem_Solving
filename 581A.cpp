a6925b74b3616298b09b7746277eeeaeba17288e
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-04-01 03:39:29 +0600

    Add files via upload

---
+++ 581A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a > b)
+        cout << b << " ";
+    else
+        cout << a << " ";
+
+    int ans = abs(a - b);
+    cout << ans / 2 << endl;
+}
