3540ea2a1ea7f575ff049cd1421c873740b1e8b4
Md. Ariful Islam <49308565+ashik255@users.noreply.github.com>
2023-04-01 20:45:53 +0600

    Add files via upload

---
+++ 450A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; double m;
+    cin >> n >> m;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        if (arr[i] <= m)
+            arr[i] = 0;
+    }
+    double maxi = 0;
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (ceil(arr[i] / m) >= maxi)
+        {
+            maxi = ceil(arr[i] / m);
+            ans = i;
+        }
+    }
+    cout << ans + 1 << endl;
+}
